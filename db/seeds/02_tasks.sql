02050925925eb3364b2553d0da4b691737bd8fec
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-20 16:24:39 -0700

    add seed for tasks and categories dbs

---
+++ db/seeds/02_tasks.sql
@@ -0,0 +1,19 @@
+-- Seed to_watch (category 1)
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 1, 'Breaking Bad', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 1, 'Arrested Development', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 1, 'Waiting for Guffman', FALSE);
+
+-- Seed to_eat (category 2)
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 2, 'Five Guys', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 2, 'Deep Fried Mars Bar', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 2, 'Milestones', FALSE);
+
+-- Seed to_read (category 3)
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 3, 'Twilight', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 3, 'Dune', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 3, 'Animal Farm', FALSE);
+
+-- Seed to_buy (category 4)
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 4, 'Socks', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 4, 'Keyboard', FALSE);
+INSERT INTO tasks (user_id, category_id, task, isComplete) VALUES (1, 4, 'Scissors', FALSE);
---
+++ db/seeds/03_categories.sql
@@ -0,0 +1,5 @@
+-- Users table seeds here (Example)
+INSERT INTO categories (category) VALUES ('to_watch');
+INSERT INTO categories (category) VALUES ('to_eat');
+INSERT INTO categories (category) VALUES ('to_read');
+INSERT INTO categories (category) VALUES ('to_buy');
