d6a158b2a8e6a6aa913ca7783501fdff4e9bd865
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-20 15:21:40 -0700

    add tasks schema

--- db/schema/02_tasks.sql
+++ db/schema/02_tasks.sql
@@ -1,9 +1,9 @@
--- Drop and recreate Widgets table (Example)
-
 DROP TABLE IF EXISTS tasks CASCADE;
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  category_id INTEGER REFERENCES categories(id),
+  task VARCHAR(255) NOT NULL,
+  isComplete BOOLEAN
 );
