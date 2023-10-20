2837666d9e9e1e6f152c951f484a599805bc5f73
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-20 15:10:50 -0700

    rename widgets in db to task and add categories db schema

--- db/schema/02_tasks.sql
+++ db/schema/02_tasks.sql
@@ -1,7 +1,8 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+DROP TABLE IF EXISTS tasks CASCADE;
+
+CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL
