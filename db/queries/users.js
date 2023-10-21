f9be83bfb5b19cb4194d867cec6d6c2b14a09978
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-21 23:32:48 +0000

    getTasksByUser and error function

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,18 @@
 const db = require('../connection');
 
+const error = clientMsg => error => console.log(clientMsg, error.message);
+
 const getUsers = () => {
   return db.query('SELECT * FROM users;')
     .then(data => {
       return data.rows;
-    });
+    })
+    .catch(error('getUsers'));
 };
 
+const getTasksByUser = (user_id) => {
+  return db.query('SELECT * FROM tasks WHERE user_id = $1', [user_id])
+    .then(data => data.rows)
+    .catch(error(`getTasksByUser ${user_id}`));
+}
 module.exports = { getUsers };
