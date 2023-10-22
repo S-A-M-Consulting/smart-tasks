15db2fb470e6003c2bd774eab26e67bc55e30ace
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-21 20:40:10 -0700

    add more ajax function to interact with backend api

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -1,6 +1,7 @@
 // Client facing scripts here
 // needs to be added through a script tag on the index.html
 
+// essential ajax functions to interact with backend API
 function getAllTasks() {
   let url = "/api/tasks";
   return $.ajax({
@@ -8,10 +9,43 @@ function getAllTasks() {
   });
 }
 
-// const login =
-// const logout =
-// const createTask =
+function logOut() {
+  return $.ajax({
+    method: "POST",
+    url: "/users/logout",
+  });
+}
+
+function logIn(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/users/login",
+    data,
+  });
+}
+
+const createTask = function (data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/tasks",
+    data,
+  });
+};
+
+const editTask = function (data) {
+  return $.ajax({
+    method: "PATCH",
+    url: "/api/tasks/:id",
+    data,
+  });
+};
+
+const deleteTask = function (data) {
+  return $.ajax({
+    method: "DELETE",
+    url: "/api/tasks/:id",
+    data,
+  });
+};
+
 // const completeTask =
-// const editTask =
-// const recategorizeTask =
-// const deleteTask =
