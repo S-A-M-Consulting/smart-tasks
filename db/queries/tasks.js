35480f0e17baecec7c0eaa0b41947975361f638a
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-23 19:07:21 +0000

    insert generic function

--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -1,5 +1,5 @@
 const db = require('../connection');
-const { error, getAll} = require('./util.js');
+const { error, getAll, insert} = require('./util.js');
 
 const getTasksByUser = (user_id) => {
   return db.query('SELECT * FROM tasks WHERE user_id = $1', [user_id])
@@ -7,9 +7,16 @@ const getTasksByUser = (user_id) => {
     .catch(error(`getTasksByUser ${user_id}`));
 }
 
-const createTask = (user_id, data) => {
+const createTask = (data) => {
+  return insert(db, 'tasks', data)
+    .catch(error('createTask'));
+}
+
+const editTask = (data) => {
   
 }
+
+module.exports = {getTasksByUser, createTask};
 
 if (process.env.debug) {
   getTasksByUser(1).then(data => console.log("1:", data));
--- db/queries/util.js
+++ db/queries/util.js
@@ -23,13 +23,25 @@ const insert = (db, table, data) => {
   return db.query(statement, Object.values(data));
 }
 
+const update = (db, table, id, newData) => {
+  const names = Object.keys(newData);
+  const values = Object.values(newData);
+
+  const writeStatements = keys => keys.map((elem, i) => `${elem} = $${i + 1}`).join(', ');
+
+  const statement = `UPDATE ${table} SET ${writeStatements(names)} WHERE id = ${id}`;
+
+  return db.query(statement, values);
+}
+
 //ONLY ACCEPT $ positions
 const where = (field, value) => `WHERE ${field} = ${value}`;
 const and   = (field, value) => `AND ${field} = ${value}`;
 
 
-module.exports = {error, getAll, getOne};
+module.exports = {error, getAll, getOne, insert, update};
 
 if (process.env.debug) {
-  console.log(insert(db, 'users', {name: 'jessie' , email: 'j@example.com', password: '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'}));
+  //console.log(insert(db, 'users', {name: 'jessie' , email: 'j@example.com', password: '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'}));
+  console.log(update(db, 'users', 4, {name: 'Jessie Ware', email: 'jw@example.com'}));
 }
