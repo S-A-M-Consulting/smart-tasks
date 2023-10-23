e4468d951e8bbfb7c21d200eb5edcbdfbf4be4d3
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-23 12:26:02 -0700

    delete index to use alberts home.html

--- public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-// initial call to set views_manager to logIn
-$(() => {
-  logIn().then(function (json) {
-    // propertyListings.addProperties(json.properties);
-    views_manager.show("logIn");
-    console.log(json);
-  });
-});
