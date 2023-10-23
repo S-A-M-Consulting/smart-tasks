097a59c2cca3e2747a51cdab9bbb145e32e7f6f2
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-23 11:46:33 -0700

    changing to mirror tweeter app style for familiarity. Ajax calls moved to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,68 @@
-// // Client facing scripts here
-// $(() => {
-//   $('#fetch-users').on('click', () => {
-//     $.ajax({
-//       method: 'GET',
-//       url: '/api/users'
-//     })
-//     .done((response) => {
-//       const $usersList = $('#users');
-//       $usersList.empty();
+$(document).ready(function () {
+  // essential ajax functions to interact with backend API
+  const getMyTasks = function (userId) {
+    let url = "/api/tasks";
+    return $.ajax({
+      url,
+      type: "GET",
+    })
+      .then((myTasks) => {
+        //renderMyTasks (or something similar)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-//       for(const user of response.users) {
-//         $(`<li class="user">`).text(user.name).appendTo($usersList);
-//       }
-//     });
-//   });
-// });
-$(document).ready(function () {});
+  const logout = function () {
+    return $.ajax({
+      method: "POST",
+      url: "/users/logout",
+    });
+  };
+
+  const logIn = function (data) {
+    return $.ajax({
+      method: "POST",
+      url: "/users/login",
+      data,
+    });
+  };
+
+  const createTask = function (data) {
+    return $.ajax({
+      method: "POST",
+      url: "/api/tasks",
+      data,
+    })
+      .then((newTask) => {
+        //renderNewTask (or something similar)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // this function can edit the task description itself aswell as mark it as complete
+  const editTask = function (taskId, data) {
+    return $.ajax({
+      method: "PATCH",
+      url: "/api/tasks/${taskId}",
+      data,
+    });
+  };
+
+  const deleteTask = function (taskId, data) {
+    return $.ajax({
+      method: "DELETE",
+      url: "/api/tasks/${taskId}",
+      data,
+    })
+      .then((deleteTask) => {
+        //renderMyTasks (or something similar)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+});
