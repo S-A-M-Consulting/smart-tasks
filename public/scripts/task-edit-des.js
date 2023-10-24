e491f03a48cb1e8a6fb01f914ba252cd28962f2c
Yiqun Huang <albert913388672@gmail.com>
2023-10-24 01:25:30 -0700

    add the wedge for the edit task des, in task-edit-des.js

--- public/scripts/task-edit-des.js
+++ public/scripts/task-edit-des.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
-  // Initial state: Show "Edit Description" and "Change Category" buttons, and hide "Save" button
-  $(".edit-button, .category-button").show();
+  // Initial state: Show "Edit Description" button and hide "Done," "Change Category," and "Save" buttons for all tasks
+  $(".done-button, .category-button, .edit-button").show();
   $(".save-button").hide();
 
   // "Edit Description" button click event
   $(".edit-button").click(function () {
-    // Hide "Edit Description" and "Change Category" buttons
-    $(".edit-button, .category-button").hide();
-    // Show "Save" button
-    $(".save-button").show();
+    // Get the task's specific ID
+    const taskId = $(this).closest(".task").attr("id");
+
+    // Hide 3 button
+    $(
+      `#${taskId} .done-button, #${taskId} .category-button, #${taskId} .edit-button`
+    ).hide();
+    // Show "Save" buttons
+    $(
+      `#${taskId} .save-button`
+    ).show();
 
     // Get the card text and create an editable textarea
     const cardText = $(this).closest(".card-body").find(".card-text");
@@ -23,17 +30,22 @@ $(document).ready(function () {
 
   // "Save" button click event
   $(".save-button").click(function () {
-    // Get the edited content from the textarea
-    const editedContent = $(this).closest(".card-body").find("textarea").val();
+    // Get the task's specific ID
+    const taskId = $(this).closest(".task").attr("id");
 
-    // Replace the textarea with a new card text containing the edited content
+    // Hide "Save" buttons
+    $(
+      `#${taskId} .save-button`
+    ).hide();
+    // Show 3 button
+    $(
+      `#${taskId} .done-button, #${taskId} .category-button, #${taskId} .edit-button`
+    ).show();
+
+    // Replace the textarea with a new card text containing the edited content (if any)
+    const editedContent = $(this).closest(".card-body").find("textarea").val();
     const cardText = $("<p>").addClass("card-text").text(editedContent);
 
     $(this).closest(".card-body").find("textarea").replaceWith(cardText);
-
-    // Show "Edit Description" and "Change Category" buttons
-    $(".edit-button, .category-button").show();
-    // Hide "Save" button
-    $(this).hide();
   });
 });
